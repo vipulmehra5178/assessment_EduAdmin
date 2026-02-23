11db36c8c23d42c52bafef827125a1d51392d49e
Vipul Mehra <vipulmehra02899@gmail.com>
2026-02-23 15:55:13 +0530

    Initial commit from Create Next App

---
+++ app/theme.js
@@ -0,0 +1,16 @@
+'use client';
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#9c27b0',
+    },
+  },
+});
+
+export default theme;
